dbbea9f405c081a4efbe4295e95eeec7a78e046f
elayachiamritoff <elayachiamritoff@gmail.com>
2024-02-06 21:08:57 +0100

    ref

--- script.rb
+++ script.rb
@@ -1,11 +1,32 @@
 class LinkedList
+    attr_acccessor :head
+
+    def initialize
+        @head = nil
+    end
+
+def append(data)
+   if @head.nil?
+         @head = Node.new(data)
+      else
+      curr_node = @head
+
+      while curr_node.next_node
+         curr_node = curr_node.next_node
+      end
+
+      curr_node.next_node = Node.new(data)
+      end
 end
 
-class Node
-    attr_accessor :value, :next_node
-    def initialize(value = nil, next_node = nil)
+    private
+
+    class Node
+     attr_acccessor :value, :next_node
+     def initialize(value = nil, next_node = nil)
         @value = value
         @next_node = next_node
-    end
-end
+     end
+     end
 
+end
