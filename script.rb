6d061f42743fd0de5422df3e4faf52770684d9c1
elayachiamritoff <elayachiamritoff@gmail.com>
2024-02-07 22:44:07 +0100

    Add at/pop method

--- script.rb
+++ script.rb
@@ -1,65 +1,99 @@
 class LinkedList
-    attr_accessor :head
-    @@total_nodes
-    def initialize
-        @head = nil
-    end
+  attr_accessor :head
 
-def append(data)
-   if @head.nil?
-         @head = Node.new(data)
-      else
+  def initialize
+    @head = nil
+  end
+
+  def append(data)
+    if @head.nil?
+      @head = Node.new(data)
+    else
       curr_node = @head
-
       while curr_node.next_node
-         curr_node = curr_node.next_node
+        curr_node = curr_node.next_node
       end
-
       curr_node.next_node = Node.new(data)
-      end
-end
+    end
+  end
 
-def prepend(data)
-   first_node = @head
-   new_node = Node.new(data)
-   new_node.next_node = first_node
-   @head = new_node
-end
+  def prepend(data)
+    first_node = @head
+    new_node = Node.new(data)
+    new_node.next_node = first_node
+    @head = new_node
+  end
 
-def size
-   count = 0
-   curr_node = @head
-   while curr_node
+  def size
+    count = 0
+    curr_node = @head
+    while curr_node
       count += 1
       curr_node = curr_node.next_node
+    end
+    count
+  end
+
+  def head
+    @head
+  end
+
+  def tail
+    return nil if @head.nil?
+
+    curr_node = @head
+    while curr_node.next_node
+      curr_node = curr_node.next_node
+    end
+    curr_node
+  end
+
+  def at(index)
+    return @head if index <= 0
+    return IndexError, "Index out of range" if index > self.size
+    curr_node = @head
+    index.times do
+      curr_node = curr_node.next_node
+    end
+    curr_node
+  end
+
+  def pop
+     if @head.next_node.nil?
+        deleted_node = @head
+        @head = nil
+        return :HEAD_REMOVED
+     else
+        curr_node = @head
+
+        # 1 > 2 > 3 > 4 > nil
+        while curr_node.next_node && curr_node.next_node.next_node
+         curr_node = curr_node.next_node
+        end
+
+    deleted_node = curr_node.next_node
+    curr_node.next_node = nil
+    return deleted_node.value
    end
 
-   count
+  end
+
+  private
+
+  class Node
+    attr_accessor :value, :next_node
+
+    def initialize(value = nil, next_node = nil)
+      @value = value
+      @next_node = next_node
+    end
+  end
 end
 
-def head
-   @head
-end
+linklist = LinkedList.new
+linklist.append(1)
+linklist.append(2)
+linklist.append(3)
 
-def tail
 
-   return nil if @head.nil?
-   curr_node = @head
-   while curr_node.next_node
-   curr_node = curr_node.next_node
-   end
 
-   curr_node
-end
-    private
-
-    class Node
-     attr_accessor :value, :next_node
-
-     def initialize(value = nil, next_node = nil)
-        @value = value
-        @next_node = next_node
-     end
-     end
-
-end
